077c18f917718899a1ce0205965267a6621286c9
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-10-05 23:27:17 +0900

    feat: use clap to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,49 @@
 use bittorrent_starter_rust::{bencode, peers, Torrent};
-use std::{env, net::SocketAddrV4, net::TcpStream, str::FromStr};
+use clap::{Parser, Subcommand};
+use std::net::TcpStream;
+
+#[derive(Clone, Debug, Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum Commands {
+    Decode {
+        encoded_value: String,
+    },
+    Info {
+        file_path: String,
+    },
+    Peers {
+        file_path: String,
+    },
+    Handshake {
+        file_path: String,
+        peer_ip: String,
+    },
+    #[command(name = "download_piece")]
+    DownloadPiece {
+        #[arg(short, long = "out")]
+        output_path: Option<String>,
+        file_path: String,
+        piece_index: usize,
+    },
+}
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+    let cli = Cli::parse();
 
-    match command.as_str() {
-        "decode" => {
-            let encoded_value = &args[2];
+    match &cli.command {
+        Commands::Decode { encoded_value } => {
             let decoded_value = bencode::decode(encoded_value.as_bytes());
             println!("{}", decoded_value);
         }
-        "info" => {
-            let file_path = &args[2];
+        Commands::Info { file_path } => {
             let torrent = Torrent::from_file(file_path).unwrap();
             println!("Tracker URL: {}", torrent.announce);
             println!("Length: {}", torrent.length);
@@ -21,25 +51,29 @@ fn main() {
             println!("Piece Length: {}", torrent.piece_length);
             println!("Pieces: \n{}", torrent.piece_hashes.join("\n"));
         }
-        "peers" => {
-            let file_path = &args[2];
+        Commands::Peers { file_path } => {
             let torrent = Torrent::from_file(file_path).unwrap();
             let torrent_peers = peers::fetch_peers(&torrent).unwrap();
 
             torrent_peers.iter().for_each(|peer| println!("{}", peer));
         }
-        "handshake" => {
-            let file_path = &args[2];
+        Commands::Handshake { file_path, peer_ip } => {
             let torrent = Torrent::from_file(file_path).unwrap();
 
-            let peer_ip = SocketAddrV4::from_str(&args[3]).unwrap();
             let mut stream = TcpStream::connect(peer_ip).unwrap();
             let result = peers::shake_hands(&mut stream, &torrent).unwrap();
 
             println!("Peer ID: {}", result);
         }
-        _ => {
-            println!("unknown command: {}", args[1])
+        Commands::DownloadPiece {
+            output_path,
+            file_path,
+            piece_index,
+        } => {
+            println!("output_path: {:?}", output_path);
+            println!("file_path: {}", file_path);
+            println!("piece_index: {}", piece_index);
+            todo!();
         }
     }
 }
